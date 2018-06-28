eb28c33f4b07ea3c77724ae6d00e8d6f06f5e547
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-28 14:32:36 -0500

    build out app component to incorporate all components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,127 +1,28 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import {Link} from 'react-router-dom'; 
-
-// Material-UI imports
-import Radio from '@material-ui/core/Radio/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Button from '@material-ui/core/Button/Button';
-import Input from '@material-ui/core/Input/Input';
-import {withStyles} from '@material-ui/core';
-
+import {HashRouter as Router, Route} from 'react-router-dom';
 // Styles
-import {styles} from './styles';
 import './App.css';
+
+// Components
+import Menu from '../Menu/Menu';
+import Customer from '../Customer/Customer';
+import Checkout from '../Checkout/Checkout';
 
 class App extends Component {
 
-  constructor(){
-    super();
-
-    this.state = {
-      name: '',
-      street_address: '',
-      city: '',
-      zip: 0,
-      type: ''    }
-  }
-
-  handleInputChange = (event) => {
-    switch(event.target.id){
-      case 'name':
-        this.setState({name: event.target.value});
-        break;
-      case 'address':
-        this.setState({street_address: event.target.value});
-        break; 
-      case 'city':
-        this.setState({city: event.target.value});
-        break;
-      case 'zip':
-        this.setState({zip: event.target.value});
-        break;
-      case 'pickup':
-        this.setState({type: event.target.value});
-        break;
-      case 'delivery':
-        this.setState({type: event.target.value});
-        break;
-      default:
-        console.log('Invalid field');
-        break;      
-    }
-  }
-
-  submitCustomerInfo = () => {
-    this.props.dispatch({type: 'ADD_CUSTOMER', payload: this.state});
-  }
-
   render(){
-    const { classes } = this.props;
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Prime Pizza</h1>
-        </header>
-        <br/>
-        <img src="images/pizza_photo.png"/>
-        <p>Pizza is great.</p>
-        <pre>{JSON.stringify(this.state)}</pre>
-        <h2>Step 2: Customer Information</h2>
-        <div className={classes.customerInputForm} noValidate autoComplete='off'>
-          <Input
-            id="name"
-            placeholder="Name"
-            className={classes.customerInput}
-            value={this.state.name}
-            onChange={this.handleInputChange}
-          />
-         <Input 
-            id="address"
-            placeholder="Street Address"
-            className={classes.customerInput}
-            value={this.state.street_address}
-            onChange={this.handleInputChange}
-          />
-          <Input 
-            id="city"
-            placeholder="City"
-            className={classes.customerInput}
-            value={this.state.city}
-            onChange={this.handleInputChange}
-          />
-          <Input 
-            id="zip"
-            placeholder="Zip"
-            className={classes.customerInput}
-            value={this.state.zip}
-            onChange={this.handleInputChange}
-          />
-        </div>
-        <div className={classes.methodSelection}>
-          <FormControl component="fieldset" required className={classes.formControl}>
-            <RadioGroup
-              className={classes.methodGroup}
-              value={this.state.method}
-            >
-              <FormControlLabel value="Pickup" control={<Radio id='pickup' onClick={this.handleInputChange}/>} label="Pickup" />
-              <FormControlLabel value="Delivery" control={<Radio id='delivery' onClick={this.handleInputChange}/>} label="Delivery" />
-            </RadioGroup>
-          </FormControl>
-        </div>
-        <div>
-          <Button className={classes.nextButton}onClick={this.submitCustomerInfo}></Button>
-        </div>
+        <Router>
+          <div>
+            <Route exact path='/' component={Menu}/>
+            <Route exact path='/customer' component={Customer}/>
+            <Route exact path='/checkout' component={Checkout}/>
+          </div>
+        </Router>
       </div>
     );
   }
 }
 
-export default compose(
-  withStyles(styles),
-  connect()
-)(App);
+export default App;
---
+++ src/components/Customer/Customer.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import {Link} from 'react-router-dom'; 
+
+// Material-UI imports
+import Radio from '@material-ui/core/Radio/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button/Button';
+import Input from '@material-ui/core/Input/Input';
+import {withStyles} from '@material-ui/core';
+
+// Styles
+import {styles} from './styles';
+
+class Customer extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      name: '',
+      street_address: '',
+      city: '',
+      zip: 0,
+      type: ''    }
+  }
+
+  handleInputChange = (event) => {
+    switch(event.target.id){
+      case 'name':
+        this.setState({name: event.target.value});
+        break;
+      case 'address':
+        this.setState({street_address: event.target.value});
+        break; 
+      case 'city':
+        this.setState({city: event.target.value});
+        break;
+      case 'zip':
+        this.setState({zip: event.target.value});
+        break;
+      case 'pickup':
+        this.setState({type: event.target.value});
+        break;
+      case 'delivery':
+        this.setState({type: event.target.value});
+        break;
+      default:
+        console.log('Invalid field');
+        break;      
+    }
+  }
+
+  submitCustomerInfo = () => {
+    this.props.dispatch({type: 'ADD_CUSTOMER', payload: this.state});
+  }
+
+  render(){
+    const { classes } = this.props;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Prime Pizza</h1>
+        </header>
+        <br/>
+        <div className={classes.customerInputForm} noValidate autoComplete='off'>
+          <Input
+            id="name"
+            placeholder="Name"
+            className={classes.customerInput}
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+         <Input 
+            id="address"
+            placeholder="Street Address"
+            className={classes.customerInput}
+            value={this.state.street_address}
+            onChange={this.handleInputChange}
+          />
+          <Input 
+            id="city"
+            placeholder="City"
+            className={classes.customerInput}
+            value={this.state.city}
+            onChange={this.handleInputChange}
+          />
+          <Input 
+            id="zip"
+            placeholder="Zip"
+            className={classes.customerInput}
+            value={this.state.zip}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <div className={classes.methodSelection}>
+          <FormControl component="fieldset" required className={classes.formControl}>
+            <RadioGroup
+              className={classes.methodGroup}
+              value={this.state.method}
+            >
+              <FormControlLabel value="Pickup" control={<Radio id='pickup' onClick={this.handleInputChange}/>} label="Pickup" />
+              <FormControlLabel value="Delivery" control={<Radio id='delivery' onClick={this.handleInputChange}/>} label="Delivery" />
+            </RadioGroup>
+          </FormControl>
+        </div>
+        <div>
+          <Button className={classes.nextButton}onClick={this.submitCustomerInfo}><Link to='/checkout'>Next</Link></Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default compose(
+  withStyles(styles),
+  connect()
+)(Customer);
